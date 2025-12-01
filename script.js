a6bb739d726ec8122592e25cfd1629529f2de677
Jephthah2 <225693441+Jephthah2@users.noreply.github.com>
2025-12-01 05:21:13 +0000

    Create a 16x16 grid using JS and flexbox

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container');
+
+//create a 16x16 grid
+for (let i = 0; i < 16; i++) {
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('gridContainer');
+
+    for (let j = 0; j < 16; j++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        gridContainer.appendChild(grid)
+    }
+    container.appendChild(gridContainer);
+}
