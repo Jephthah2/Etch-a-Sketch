aea98fc0b76228919e8fbed98c7d9f31b333927f
Jephthah2 <225693441+Jephthah2@users.noreply.github.com>
2025-12-17 04:37:03 +0000

    Add button on top of the screen

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
-
+        
+        // Add a hover effect
         grid.addEventListener('mouseover', (event) => {
             event.target.style.backgroundColor = "blue";
         });
@@ -16,4 +17,22 @@ for (let i = 0; i < 16; i++) {
         gridContainer.appendChild(grid);
     }
     container.appendChild(gridContainer);
+}
+
+//Add a button on the top of the screen
+const button = document.createElement("button");
+button.classList.add("grid-size-btn");
+button.textContent = "Change grid size";
+container.insertBefore(button, container.firstElementChild);
+
+//Check inout for grid size
+function checkValidity(number) {
+    if(!Number.isInteger(number)) {
+        alert("Please enter a valid number")
+    }
+    if(number > 100) {
+        alert("Enter a number less than 100")
+    }
+}   if(number < 0) {
+    alert("Enter a positive number")
 }
